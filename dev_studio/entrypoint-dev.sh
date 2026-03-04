baa891a0326c8f3107f44cd90579edffde2f51d5
salmajnouni <salmajnouni@gmail.com>
2026-03-04 14:26:33 -0500

    fix: إصلاح entrypoint-dev.sh - استبدال heredoc بـ printf لتجنب خطأ -c

--- dev_studio/entrypoint-dev.sh
+++ dev_studio/entrypoint-dev.sh
@@ -1,12 +1,7 @@
-#!/bin/bash
-set -e
+#!/bin/sh
 # كتابة كلمة المرور من متغير البيئة PASSWORD في config.yaml
 mkdir -p /home/coder/.config/code-server
-cat > /home/coder/.config/code-server/config.yaml << YAML
-bind-addr: 0.0.0.0:8080
-auth: password
-password: ${PASSWORD}
-cert: false
-YAML
+printf 'bind-addr: 0.0.0.0:8080\nauth: password\npassword: %s\ncert: false\n' "$PASSWORD" \
+    > /home/coder/.config/code-server/config.yaml
 # تشغيل code-server
 exec code-server --config /home/coder/.config/code-server/config.yaml /home/coder/SaleHSaaS3
